f7bca6c80e4fcd22540826e8937247d8e116e370
Andrey7mirnov <andrey7mirnov@gmail.com>
2023-02-19 14:52:12 +0300

    add methods checking

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,3 +1,5 @@
+import json
+
 from requests import Response
 
 from utils.api import Google_maps_api
@@ -14,23 +16,50 @@ class Test_create_place():
         check_post = result_post.json()
         place_id = check_post.get("place_id")
         Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        # token = json.loads(result_post.text)
+        # print(list(token))
+        Checking.check_json_value(result_post, "status", "OK")
 
         print("Метод GET POST")
         result_get: Response = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
+        # token = json.loads(result_get.text)
+        # print(list(token))
+        Checking.check_json_value(result_get, "address", "29, side layout, cohen 09")
 
         print("Метод PUT")
         result_put: Response = Google_maps_api.put_new_place(place_id)
         Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ['msg'] )
+        # token = json.loads(result_put.text)
+        # print(list(token))
+        Checking.check_json_value(result_put, "msg", "Address successfully updated")
 
         print("Метод GET PUT")
         result_get: Response = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
+        # token = json.loads(result_get.text)
+        # print(list(token))
+        Checking.check_json_value(result_get, "address", "100 Lenina street, RU")
 
         print("Метод DELETE")
         result_delete: Response = Google_maps_api.delete_new_place(place_id)
         Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_delete, ['status'])
+        # token = json.loads(result_delete.text)
+        # print(list(token))
+        Checking.check_json_value(result_delete, "status", "OK")
 
         print("Метод GET DELETE")
         result_get: Response = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ['msg'])
+        # token = json.loads(result_get.text)
+        # print(list(token))
+        Checking.check_json_search_word_in_value(result_get, "msg", "failed")
+        Checking.check_json_value(result_get, "msg", "Get operation failed, looks like place_id  doesn't exists")
+
+        print("Тестирование создания, изменения и удаления прошло успешно")
--- utils/checking.py
+++ utils/checking.py
@@ -1,4 +1,6 @@
 """Методы для проверки ответов наших запросов"""
+import json
+
 from requests import Response
 
 
@@ -11,3 +13,25 @@ class Checking():
             print("Успешно!Статус код = " + str(response.status_code))
         else:
             print("Провал! Статус код = " + str(response.status_code))
+
+    @staticmethod
+    def check_json_token(response: Response, expected_value):
+        token = json.loads(response.text)
+        assert list(token) == expected_value
+        print("Все поля присутствуют")
+
+    @staticmethod
+    def check_json_value(response:Response, field_name, expected_value):
+        check = response.json()
+        check_info = check.get(field_name)
+        assert check_info == expected_value
+        print(field_name + " верен!!!")
+
+    @staticmethod
+    def check_json_search_word_in_value(response: Response, field_name, search_word):
+        check = response.json()
+        check_info = check.get(field_name)
+        if search_word in check_info:
+            print("Слово " + search_word + " присутствует")
+        else:
+            print("Слово " + search_word + " отсутствует")
